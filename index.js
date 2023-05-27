d2f6b1ec2f41623a0fe8fc715ea7d8b8e7a7b23a
Matty <mattwonus@gmail.com>
2023-05-27 13:35:06 -0400

    Added serverlocation variable

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//SERVER LOCATION
+var serverloc = "/home/pzserver/Zomboid/Server/"
+
 //FS
 const fs = require('fs');
 
@@ -85,7 +88,7 @@ var map = 'Map = {AllowMiniMap = false,AllowWorldMap = true,MapAllKnown = false,
 var ZombieLore = 'ZombieLore = {Cognition = 3,ThumpNoChasing = false,ThumpOnConstruction = true,TriggerHouseAlarm = false,ZombiesDragDown = true,ZombiesFenceLunge = true,' + "Speed = " + Speed + "," + "Strength = " + Strength + "," + "Toughness = " + Toughness + "," + "Transmission = " + Transmission + "," + "Mortality = " + Mortality + "," + "Reanimate = " + Reanimate + "," + "CrawlUnderVehicle = " + CrawlUnderVehicle + "," + "Memory = " + Memory + "," + "Sight = " + Sight + "," + "Hearing = " + Hearing + "," + "ActiveOnly = " + ActiveOnly + "," + "DisableFakeDead = " + DisableFakeDead + ",},"
 var ZombieConfig = 'ZombieConfig = {RespawnHours = 72.0,RespawnUnseenHours = 16.0,RespawnMultiplier = 0.1,RedistributeHours = 12.0,FollowSoundDistance = 100,RallyGroupSize = 20,RallyTravelDistance = 20,RallyGroupSeparation = 15,RallyGroupRadius = 3,' + "PopulationMultiplier = " + PopulationMultiplier + "," + "PopulationStartMultiplier = " + PopulationStartMultiplier + "," + "PopulationPeakMultiplier = " + PopulationPeakMultiplier + "," + "PopulationPeakDay = " + PopulationPeakDay + ",},"
 var luaCode = hardcode + variables + map + ZombieLore + ZombieConfig + '}'
-var luaPath = 'servertest_SandboxVars.lua'
+var luaPath = serverloc+'servertest_SandboxVars.lua'
 
 //Write servertest_SandboxVars.lua
 fs.writeFile(luaPath, luaCode, (err) => {
@@ -97,7 +100,7 @@ fs.writeFile(luaPath, luaCode, (err) => {
 });
 
 //Load Variables for servertest.ini from mem.json
-const mem = JSON.parse(fs.readFileSync('./mem.json',{ encoding:'utf8', flag: 'r'}));
+const mem = JSON.parse(fs.readFileSync(serverloc+'mem.json',{ encoding:'utf8', flag: 'r'}));
 var WipeNumber = mem.memory.wipenumber
 var ResetID = mem.memory.resetid
 var PlayerID = mem.memory.playerid
@@ -106,7 +109,7 @@ var PlayerID = mem.memory.playerid
 var iniinc = 'ServerWelcomeMessage=BROTON PROJECT RANDBOID <LINE> <LINE> We are on wipe number '+WipeNumber+'\r\n'+'ResetID='+ResetID+'\r\n'+'ServerPlayerID='+PlayerID
 var inistatic = '\r\nPVP=true\r\nPauseEmpty=true\r\nGlobalChat=true\r\nChatStreams=s,r,a,w,y,sh,f,all\r\nOpen=true\r\nAutoCreateUserInWhiteList=false\r\nDisplayUserName=true\r\nShowFirstAndLastName=false\r\nSpawnPoint=0,0,0\r\nSafetySystem=true\r\nShowSafety=true\r\nSafetyToggleTimer=2\r\nSafetyCooldownTimer=3\r\nSpawnItems=\r\nDefaultPort=16261\r\nUDPPort=16262\r\nMods=\r\nMap=Muldraugh, KY\r\nDoLuaChecksum=true\r\nDenyLoginOnOverloadedServer=true\r\nPublic=false\r\nPublicName=BROTON PROJECT RANDBOID\r\nPublicDescription=Server auto resets on the 1st of each month. Sever config files are automatically randomized. Factions & Safehouse are on.\r\nMaxPlayers=32\r\nPingLimit=400\r\nHoursForLootRespawn=0\r\nMaxItemsForLootRespawn=4\r\nConstructionPreventsLootRespawn=true\r\nDropOffWhiteListAfterDeath=false\r\nNoFire=false\r\nAnnounceDeath=true\r\nMinutesPerPage=0.25\r\nSaveWorldEveryMinutes=0\r\nPlayerSafehouse=true\r\nAdminSafehouse=false\r\nSafehouseAllowTrepass=true\r\nSafehouseAllowFire=true\r\nSafehouseAllowLoot=true\r\nSafehouseAllowRespawn=true\r\nSafehouseDaySurvivedToClaim=0\r\nSafeHouseRemovalTime=144\r\nSafehouseAllowNonResidential=true\r\nAllowDestructionBySledgehammer=true\r\nSledgehammerOnlyInSafehouse=false\r\nKickFastPlayers=false\r\nRCONPort=27015\r\nRCONPassword=\r\nDiscordEnable=false\r\nDiscordToken=\r\nDiscordChannel=\r\nDiscordChannelID=\r\nPassword=HelloBrother\r\nMaxAccountsPerUser=0\r\nAllowCoop=true\r\nSleepAllowed=false\r\nSleepNeeded=false\r\nKnockedDownAllowed=true\r\nSneakModeHideFromOtherPlayers=true\r\nWorkshopItems=\r\nSteamScoreboard=true\r\nSteamVAC=true\r\nUPnP=true\r\nVoiceEnable=true\r\nVoiceMinDistance=10.0\r\nVoiceMaxDistance=100.0\r\nVoice3D=true\r\nSpeedLimit=70.0\r\nLoginQueueEnabled=false\r\nLoginQueueConnectTimeout=60\r\nserver_browser_announced_ip=\r\nPlayerRespawnWithSelf=false\r\nPlayerRespawnWithOther=false\r\nFastForwardMultiplier=40.0\r\nDisableSafehouseWhenPlayerConnected=false\r\nFaction=true\r\nFactionDaySurvivedToCreate=0\r\nFactionPlayersRequiredForTag=1\r\nDisableRadioStaff=false\r\nDisableRadioAdmin=true\r\nDisableRadioGM=true\r\nDisableRadioOverseer=false\r\nDisableRadioModerator=false\r\nDisableRadioInvisible=true\r\nClientCommandFilter=-vehicle.*;+vehicle.damageWindow;+vehicle.fixPart;+vehicle.installPart;+vehicle.uninstallPart\r\nClientActionLogs=ISEnterVehicle;ISExitVehicle;ISTakeEngineParts;\r\nPerkLogs=true\r\nItemNumbersLimitPerContainer=0\r\nBloodSplatLifespanDays=0\r\nAllowNonAsciiUsername=false\r\nBanKickGlobalSound=true\r\nRemovePlayerCorpsesOnCorpseRemoval=false\r\nTrashDeleteAll=true\r\nPVPMeleeWhileHitReaction=false\r\nMouseOverToSeeDisplayName=true\r\nHidePlayersBehindYou=true\r\nPVPMeleeDamageModifier=30.0\r\nPVPFirearmDamageModifier=50.0\r\nCarEngineAttractionModifier=0.5\r\nPlayerBumpPlayer=false\r\nMapRemotePlayerVisibility=2\r\nBackupsCount=5\r\nBackupsOnStart=true\r\nBackupsOnVersionChange=true\r\nBackupsPeriod=0\r\nAntiCheatProtectionType1=true\r\nAntiCheatProtectionType2=true\r\nAntiCheatProtectionType3=true\r\nAntiCheatProtectionType4=true\r\nAntiCheatProtectionType5=true\r\nAntiCheatProtectionType6=true\r\nAntiCheatProtectionType7=true\r\nAntiCheatProtectionType8=true\r\nAntiCheatProtectionType9=true\r\nAntiCheatProtectionType10=true\r\nAntiCheatProtectionType11=true\r\nAntiCheatProtectionType12=true\r\nAntiCheatProtectionType13=true\r\nAntiCheatProtectionType14=true\r\nAntiCheatProtectionType15=true\r\nAntiCheatProtectionType16=true\r\nAntiCheatProtectionType17=true\r\nAntiCheatProtectionType18=true\r\nAntiCheatProtectionType19=true\r\nAntiCheatProtectionType20=true\r\nAntiCheatProtectionType21=true\r\nAntiCheatProtectionType22=true\r\nAntiCheatProtectionType23=true\r\nAntiCheatProtectionType24=true\r\nAntiCheatProtectionType2ThresholdMultiplier=3.0\r\nAntiCheatProtectionType3ThresholdMultiplier=1.0\r\nAntiCheatProtectionType4ThresholdMultiplier=1.0\r\nAntiCheatProtectionType9ThresholdMultiplier=1.0\r\nAntiCheatProtectionType15ThresholdMultiplier=1.0\r\nAntiCheatProtectionType20ThresholdMultiplier=1.0\r\nAntiCheatProtectionType22ThresholdMultiplier=1.0\r\nAntiCheatProtectionType24ThresholdMultiplier=6.0'
 var iniCode = iniinc+inistatic
-var iniPath = 'servertest.ini'
+var iniPath = serverloc+'servertest.ini'
 
 //Write servertest.ini
 fs.writeFile(iniPath, iniCode, (err) => {
@@ -122,7 +125,7 @@ var updateWipeNumber = WipeNumber+1
 var updateResetID = ResetID+1
 var updatePlayerID = PlayerID+1
 var jsonCode = '{"memory":{"wipenumber":'+updateWipeNumber+',"resetid":'+updateResetID+',"playerid":'+updatePlayerID+'}}'
-var jsonPath = 'mem.json'
+var jsonPath = serverloc+'mem.json'
 
 //Write mem.json
 fs.writeFile(jsonPath, jsonCode, (err) => {
